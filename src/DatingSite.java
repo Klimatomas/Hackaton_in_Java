4f9d3517e83489345dfbbe9b9b97ae3b2f463c59
Tomas <mrklimatomas@gmail.com>
2016-10-06 21:06:40 +0200

    Cosmetic updates

--- src/DatingSite.java
+++ src/DatingSite.java
@@ -1,43 +1,5 @@
 
-import java.util.Calendar;
-
-import java.util.InputMismatchException;
 import java.util.Scanner;
-
-class User {
-    private int year;
-    private String firstName;
-    private String lastName;
-    private String nickname;
-
-    User(int year, String firstname, String lastname) {
-        this.year = year;
-        this.firstName = firstname;
-        this.lastName = lastname;
-        this.nickname = makeUsername();
-        printUser();
-    }
-
-
-    static boolean checkAge(int year) {
-        int currentYear = Calendar.getInstance().get(Calendar.YEAR);
-        if (currentYear - year < 18) {
-            System.out.print("You must be over  18 to access this website.");
-            System.exit(0);
-        }
-        return true;
-    }
-
-    private String makeUsername() {
-        System.out.printf("%s%s\n", firstName.substring(0, 2), lastName.substring(0, 2));
-        nickname = firstName.substring(0, 2) + lastName.substring(0, 2);
-        return nickname;
-    }
-
-    private void printUser() {
-        System.out.printf("Welcome! Here are your credentials:\nFirst Name: %s\nLast Name: %s\nNickname: %s\nYear of Birth: %s", firstName, lastName, nickname, year);
-    }
-}
 
 class DatingSite {
     public static void main(String[] args) {
@@ -45,20 +7,21 @@ class DatingSite {
         int year;
         System.out.println("Please enter your year of birth:");
 
-        do
-        {
+        do {
             try {
                 String s = input.nextLine();
                 year = Integer.parseInt(s);
                 break;
-            }
-            catch (Exception e)
-            {
+            } catch (NumberFormatException e) {
                 System.out.println("You must enter a number!");
             }
         }
         while (true);
-        User.checkAge(year);
+
+        if (!User.checkAge(year)) {
+            System.out.println("You must be over  18 to visit this website!");
+            return;
+        }
 
         System.out.println("Please enter your first name:");
         String firstName = input.next();
@@ -66,6 +29,7 @@ class DatingSite {
         System.out.println("Please enter your surname");
         String lastName = input.next();
 
-        new User(year, firstName, lastName);
+        User user = new User(year, firstName, lastName);
+        user.printUser();
     }
 }
--- src/User.java
+++ src/User.java
@@ -2,35 +2,30 @@ import java.util.Calendar;
 
 class User {
     private int year;
-    private String firstName;
-    private String lastName;
+    private String firstname;
+    private String lastname;
     private String nickname;
 
     User(int year, String firstname, String lastname) {
         this.year = year;
-        this.firstName = firstname;
-        this.lastName = lastname;
+        this.firstname = firstname;
+        this.lastname = lastname;
         this.nickname = makeUsername();
-        printUser();
     }
 
 
     static boolean checkAge(int year) {
         int currentYear = Calendar.getInstance().get(Calendar.YEAR);
-        if (currentYear - year < 18) {
-            System.out.print("You must be over  18 to access this website.");
-            System.exit(0);
-        }
-        return true;
+        return (currentYear - year > 18);
     }
 
     private String makeUsername() {
-        System.out.printf("%s%s\n", firstName.substring(0, 2), lastName.substring(0, 2));
-        nickname = firstName.substring(0, 2) + lastName.substring(0, 2);
+        System.out.printf("%s%s\n", firstname.substring(0, 2), lastname.substring(0, 2));
+        nickname = firstname.substring(0, 2) + lastname.substring(0, 2);
         return nickname;
     }
 
-    private void printUser() {
-        System.out.printf("Welcome! Here are your credentials:\nFirst Name: %s\nLast Name: %s\nNickname: %s\nYear of Birth: %s", firstName, lastName, nickname, year);
+    void printUser() {
+        System.out.printf("Welcome! Here are your credentials:\nFirst Name: %s\nLast Name: %s\nNickname: %s\nYear of Birth: %s", firstname, lastname, nickname, year);
     }
 }
